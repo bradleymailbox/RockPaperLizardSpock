dce829e201925a558191a257a5edb4aa9c430a49
Bradley Stevens <Bradley Stevens@DESKTOP-2O2SVEJ>
2018-12-31 16:05:05 +0200

    Version 1

---
+++ RockPaperLizardSpock/RockPaperLizardSpock.py
@@ -0,0 +1,4 @@
+# this is the classic game of rock paper scissors lizard spock as 
+# originally mentioned in the sitcom big bang theory
+# developing my own version as I am learning python
+print "Welcome to Rock Paper Lizard Spock"
