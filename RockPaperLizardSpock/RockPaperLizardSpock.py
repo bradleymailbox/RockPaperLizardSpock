18205d72416cc5d0bebccfb98edba0e52029ac69
Bradley Stevens <Bradley Stevens@DESKTOP-2O2SVEJ>
2019-01-02 12:11:17 +0200

    Release version 1. Program fully functional. Implemented a separated function module.

--- RockPaperLizardSpock/RockPaperLizardSpock.py
+++ RockPaperLizardSpock/RockPaperLizardSpock.py
@@ -5,5 +5,49 @@
 # code modules
 import codemodule
 
-codemodule.printmsg("Welcome to Rock Paper Lizard Spock")
+#init variables
+userselect = 0
+userscore  = 0
+computerscore = 0
+
+while userselect != 9:
+    #show the menu
+    codemodule.showMenu()
+    #get the user selection
+    userselect = int(input())
+    #check the selection and determine the action
+    if(userselect not in range(1,6)):
+        if (userselect == 9):
+            print("Thanks for playing - Bye!")
+            exit
+        else:
+            print("Invalid selection made! - Only enter 1 to 5")
+    else:
+        #get the computer selection
+        computerselect = codemodule.computerSelects();
+        #print the selections
+        print("User selected:", codemodule.getSelectionName(int(userselect)))
+        print("Computer selected:", codemodule.getSelectionName(int(computerselect)))
+        #get the winner
+        winner = codemodule.getWinner(userselect,computerselect )
+        #print the winner
+        if(winner!="stale"):
+            print(winner, " Wins!")
+        else:
+            print(winner, " No one wins!")
+
+        if (winner == "user"):
+            userscore += 1
+        elif (winner == "computer"):
+            computerscore +=1
+        #print the user score
+        codemodule.printScore(userscore, computerscore)
+
+
+
+
+
+
+
+
 
--- RockPaperLizardSpock/codemodule.py
+++ RockPaperLizardSpock/codemodule.py
@@ -1,3 +1,67 @@
-# displays a message to user
-def printmsg(x):
-    print x
+import random
+
+rockbeats = tuple((3, 4))
+paperbeats = tuple((1,5 ))
+scissorsbeats = tuple((2, 4))
+lizardbeats = tuple((2,5))
+spockbeats= tuple((1, 3))
+
+def showMenu():
+    print("What do you choose - Enter a selection 1 to 5")
+    print("=========================================================")
+    print("1. Rock - 2. Paper - 3. Scissors - 4. Lizard - 5. Spock")
+    print("9 to Quit!")
+    print("=========================================================")
+
+def computerSelects():
+    r= random.randint(1, 5)
+    #print(r)
+    return r
+
+
+def getSelectionName(nsel):
+    if(nsel==1):
+        return "Rock"
+    elif(nsel==2):
+        return "Paper"
+    elif(nsel==3):
+        return "Scissors"
+    elif(nsel==4):
+        return "Lizard"
+    elif(nsel==5):
+        return "Spock"
+    
+
+def getWinner(user, comp):
+    if (user == comp):
+        winner = "stale"
+    elif (user == 1):
+        if (comp in rockbeats):
+            winner = "user"
+        else:
+            winner = "computer"
+    elif (user == 2):
+        if (comp in paperbeats):
+            winner = "user"
+        else:
+            winner = "computer"
+    elif (user == 3):
+        if (comp in scissorsbeats):
+            winner = "user"
+        else:
+            winner = "computer"
+    elif (user == 4):
+        if (comp in lizardbeats):
+            winner = "user"
+        else:
+            winner = "computer"
+    elif (user == 5):
+        if (comp in spockbeats):
+            winner = "user"
+        else:
+            winner = "computer"
+    return winner
+
+def printScore(user, comp):
+    print ("**USER WINS**[",user, "]**COMPUTER WINS**[",comp,"]")
+
